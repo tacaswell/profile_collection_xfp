697616935a33b332376bc0376c36cfdc5edbc3a1
Thomas A Caswell <tcaswell@bnl.gov>
2016-11-03 12:23:18 -0400

    ENH: fill in sample motor

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -9,20 +9,20 @@ class TwoButtonShutter(Device):
     # the vlaue comingout of the PV do not match what is shown in CSS
     open_cmd = Cpt(EpicsSignal, 'Cmd:In-Cmd')
     open_val = 'Inserted'
-    
+
     close_cmd = Cpt(EpicsSignal, 'Cmd:Out-Cmd')
     close_val = 'Not Inserted'
-    
+
     status = Cpt(EpicsSignalRO, 'Pos-Sts', string=True)
 
     # user facing commands
     open_str = 'Open'
     close_str = 'Close'
-    
+
     def set(self, val):
         if self._set_st is not None:
             raise RuntimeError('trying to set while a set is in progress')
-        
+
         cmd_map = {self.open_str: self.open_cmd,
                    self.close_str: self.close_cmd}
         target_map = {self.open_str: self.open_val,
@@ -33,7 +33,7 @@ class TwoButtonShutter(Device):
 
         st = self._set_st = DeviceStatus(self)
         enums = self.status.enum_strs
-        
+
         def shutter_cb(value, **kwargs):
             value = enums[int(value)]
             if value == target_val:
@@ -43,14 +43,14 @@ class TwoButtonShutter(Device):
 
         cmd_sig.set(1)
         self.status.subscribe(shutter_cb)
-        
+
         return st
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._set_st = None
         self.read_attrs = ['status']
-        
+
 shutter = TwoButtonShutter('XF:17BMA-EPS{Sh:1}', name='shutter')
 
 class Slits(Device):
@@ -70,20 +70,64 @@ class TopOutSlits(Device):
     outboard = Cpt(EpicsMotor, 'O}Mtr')
 
 
+
 class SamplePump(Device):
-    ...
+    vel = Cpt(EpicsSignal, 'Val:Vel-SP')
+    vol = Cpt(EpicsSignal, 'Val:Vol-SP')
+
+    sts = Cpt(EpicsSignal, 'Sts:Flag-Sts')
+
+    slew_cmd = Cpt(EpicsSignal, 'Cmd:Slew-Cmd')
+    stop_cmd = Cpt(EpicsSignal, 'Cmd:Stop-Cmd')
+    movr_cmd = Cpt(EpicsSignal, 'Cmd:MOVR-Cmd')
+
+    sts = Cpt(EpicsSignal, 'Sts:Flag-Sts', string=True)
+
+    def kickoff(self):
+        st = DeviceStatus(self)
+        enums = self.sts.enum_strs
+        def inner_cb(value, old_value, **kwargs):
+            old_value, value = enums[int(old_value)], enums[int(value)]
+
+            if value == 'Moving':
+                st._finished(success=True)
+                self.sts.clear_sub(inner_cb)
+
+        self.sts.subscribe(inner_cb)
+        self.slew_cmd.put(1)
+        return st
+
+    def complete(self):
+        st = DeviceStatus(self)
+        enums = self.sts.enum_strs
+        def inner_cb(value, old_value, **kwargs):
+            old_value, value = enums[int(old_value)], enums[int(value)]
+
+            if value == 'Stopped':
+                st._finished(success=True)
+                self.sts.clear_sub(inner_cb)
+
+        self.sts.subscribe(inner_cb)
+
+        self.stop_cmd.put(1)
+        return st
+
+    def stop(self):
+        self.stop_cmd.put(1)
 
 class FractionCollector(Device):
     ...
 
-sample_pump = SamplePump('', name='sample_pump')
+sample_pump = SamplePump('XF:17BMA-ES:1{Pmp:02}',
+                         name='sample_pump',
+                         read_attrs=['vel', 'vol', 'sts'])
 fraction_collector = SamplePump('', name='fraction_collector')
-    
+
 class Pump(Device):
     # This needs to be turned into a PV positioner
     mode = Cpt(EpicsSignal, 'Mode', string=True)
     direction = Cpt(EpicsSignal, 'Direction', string=True)
-    
+
     diameter = Cpt(EpicsSignal, 'Diameter_RBV', write_pv='Diameter')
     infusion_rate = Cpt(EpicsSignal, 'InfusionRate_RBV', write_pv='InfusionRate')
     run = Cpt(EpicsSignal, 'Run', string=True)
@@ -91,7 +135,7 @@ class Pump(Device):
     infusion_volume = Cpt(EpicsSignal, 'InfusionVolume_RBV', write_pv='InfusionVolume')
 
     delivered = Cpt(EpicsSignalRO, 'Delivered_RBV')
-                    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._kickoff_st = None
@@ -106,17 +150,17 @@ class Pump(Device):
         self._kickoff_st = st = DeviceStatus(self)
         def inner_cb(value, old_value, **kwargs):
             old_value, value = enums[int(old_value)], enums[int(value)]
-                     
+
             if value == 'Interrupted':
                 st._finished(success=False)
                 self.state.clear_sub(inner_cb)
-                     
+
             if value in {'Infusing', 'Withdrawing'}:
                 st._finished(success=True)
                 self.state.clear_sub(inner_cb)
 
         self.state.subscribe(inner_cb, run=False)
-        
+
         self.run.set('Run')
         return st
 
@@ -127,22 +171,22 @@ class Pump(Device):
             raise RuntimeError('must kickoff before completing')
         if self._complete_st is not None:
             raise RuntimeError('trying to complete before previous complete done')
-        
+
         def inner_cb(value, old_value, **kwargs):
             old_value, value = enums[int(old_value)], enums[int(value)]
-            
+
             if value == 'Idle' and old_value != 'Idle':
                 st._finished(success=True)
                 self.state.clear_sub(inner_cb)
                 self._kickoff_st = None
                 self._complete_st = None
-                
+
             if value == 'Interrupted':
                 st._finished(success=False)
                 self.state.clear_sub(inner_cb)
                 self._kickoff_st = None
                 self._complete_st = None
-                
+
         self.state.subscribe(inner_cb, run=self._kickoff_st.done)
         return st
 
@@ -152,11 +196,11 @@ class Pump(Device):
 
         if self._complete_st is not None:
             self._complete_st._finished(success=success)
-            
+
         self._complete_st = None
         self._kickoff_st = None
         self.run.set('Stop')
-    
+
 pump1 = Pump('XF:17BM-ES:1{Pmp:01}', name='food_pump')
 
 #pbslits = Slits('XF:17BMA-OP{Slt:PB-Ax:', name='pbslits')
@@ -177,10 +221,10 @@ def run_the_pump(pmp, **kwargs):
     yield from bp.complete(pmp, group='pump_done')
     yield from bp.wait(group='pump_done')
     yield from bp.abs_set(shutter, 'close', wait=True)
-    
+
 
 def simple_pump(pump):
-    
+
     @bp.run_decorator(md={'plan_name': 'simple_pump'})
     def inner_plan():
         yield from bp.clear_checkpoint()
@@ -188,31 +232,31 @@ def simple_pump(pump):
 
         yield from bp.kickoff(pump, group='pump_started')
         print('waiting for pump to start')
-        
+
         yield from bp.wait('pump_started')
         print('pump started')
         st = yield from bp.complete(pump, group='pump_done', wait=False)
         print('waiting for pump to finish')
-        
+
         yield from bp.trigger_and_read([pump])
-              
+
         while st is not None and not st.done:
             yield from bp.trigger_and_read([pump])
             yield from bp.sleep(.5)
-            
-        yield from bp.sleep(.1)            
+
+        yield from bp.sleep(.1)
         yield from bp.trigger_and_read([pump])
-        
+
         yield from bp.wait('pump_done')
         print('pump finished')
-        
+
         yield from bp.abs_set(shutter, 'Close', wait=True)
         print('closed shutter')
 
     def clean_up():
         yield from bp.abs_set(shutter, 'Close', wait=True)
-        
-        
+
+
     yield from bp.finalize_wrapper(inner_plan(), clean_up())
 
 
@@ -238,21 +282,21 @@ def in_vivo(food_pump, sample_pump, fraction_collector, shutter):
         yield from bp.trigger_and_read(dets)
 
         yield from bp.sleep(pre_flow_time)
-        
+
         #open the shutter
         yield from bp.abs_set(shutter, 'Open', wait=True)
         yield from bp.trigger_and_read(dets)
-        
+
         # collect some sample with beam
         yield from bp.sleep(exposure_time)
-        
+
         # close the shutter and stop flowing the sample
         yield from bp.abs_set(shutter, 'Close', wait=True)
         yield from bp.complete(sample_pump, wait=True)
         yield from bp.complete(fraction_collector, wait=True)
-        
+
         yield from bp.trigger_and_read(dets)
-        
+
         # feed the cells
         yield from bp.kickoff(food_pump, wait=True)
         yield from bp.complete(food_pump, wait=True)
@@ -266,25 +310,25 @@ def in_vivo(food_pump, sample_pump, fraction_collector, shutter):
         yield from bp.kickoff(fraction_collector, wait=True)
         yield from bp.kickoff(sample_pump, wait=True)
         yield from bp.trigger_and_read(dets)
-        
+
         # collect some sample with beam
         yield from bp.sleep(post_growth_exposure_time)
-        
+
         # close the shutter and stop flowing the sample
         yield from bp.abs_set(shutter, 'Close', wait=True)
         yield from bp.complete(sample_pump, wait=True)
         yield from bp.complete(fraction_collector, wait=True)
-        
+
         yield from bp.trigger_and_read(dets)
 
     def clean_up():
         yield from bp.abs_set(shutter, 'Close', wait=True)
         yield from bp.complete(sample_pump, wait=True)
         yield from bp.complete(fraction_collector, wait=True)
-        
+
     yield from bp.finalize_wrapper(inner_plan(), clean_up())
 
-    
+
 def print_summary(plan):
     """Print summary of plan
 
